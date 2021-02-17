bb48d5f630d0dbf6baed0f4e7cb9e3574cb28a34
Jon Jackson <ocskier@gmail.com>
2021-02-17 14:30:41 -0500

    refactor: update pkgs, update to seq v6, add dotenv

--- models/user.js
+++ models/user.js
@@ -2,15 +2,15 @@
 // sometimes causes errors on Windows machines
 var bcrypt = require("bcrypt-nodejs");
 // Creating our User model
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // Set requirements for first and last name
     name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1, 50]
-      }
+        len: [1, 50],
+      },
     },
     // The email cannot be null, and must be a proper email before creation
     email: {
@@ -18,34 +18,34 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true
-      }
+        isEmail: true,
+      },
     },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     subject: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     grade: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      allowNull: false,
+    },
     // region: {
     //   type: DataTypes.STRING,
     //   allowNull: false
     // }
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.hook("beforeCreate", function(user) {
+  User.addHook("beforeCreate", function (user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
@@ -53,7 +53,7 @@ module.exports = function(sequelize, DataTypes) {
     );
   });
 
-  User.associate = function(models) {
+  User.associate = function (models) {
     User.belongsToMany(models.Plan, { through: models.UserPlan });
   };
   return User;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 // Requiring necessary npm packages
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -27,8 +28,8 @@ require("./routes/authRoutes.js")(app);
 require("./routes/apiRoutes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
